6830bcf146849afe2f5376488c886f1305f971ec
JJimini <min102602@pusan.ac.kr>
2024-09-29 00:03:37 +0900

    fix: roles 관련 sql문 수정

---
+++ script/240929-init.sql
@@ -0,0 +1,30 @@
+\connect player_db;
+
+DROP table if exists public.player;
+
+CREATE TABLE public.player (
+	id serial4 NOT NULL,
+	nickname varchar(45) NOT NULL,
+	"password" varchar(45) NOT NULL,
+	is_deleted bool NOT NULL DEFAULT false,
+	create_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	update_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	roles _varchar NULL DEFAULT '{ROLE_GUEST}'::character varying[],
+	CONSTRAINT player_nickname_key UNIQUE (nickname),
+	CONSTRAINT player_pkey PRIMARY KEY (id)
+);
+
+\connect ranking_db;
+
+DROP table if exists public.ranking;
+
+CREATE TABLE public.ranking (
+	id serial4 NOT NULL,
+	max_score int4 NOT NULL,
+	rank_number int4 NOT NULL,
+	player_id int4 NOT NULL,
+	is_deleted bool NOT NULL DEFAULT false,
+	create_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	update_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT ranking_pkey PRIMARY KEY (id)
+);
